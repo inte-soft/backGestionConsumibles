80c4e6d0418c16f9f5bb07acd033f50965fdedf5
alejo8903 <111379163+alejo8903@users.noreply.github.com>
2023-11-14 18:38:11 -0500

    Add files via upload

---
+++ src/test/java/com/intesoft/syncworks/SyncworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intesoft.syncworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
