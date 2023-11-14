06e8306920c19a4514e4401e03215b4842d83894
alejo8903 <alejo890327@gmail.com>
2023-11-14 18:30:21 -0500

    first commit

---
+++ src/main/java/com/intesoft/syncworks/SyncworksApplication.java
@@ -0,0 +1,13 @@
+package com.intesoft.syncworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncworksApplication.class, args);
+	}
+
+}
